5050bd37fe85c6d4f576a941245d93654c270eb4
Maksym Solomko <max.exc7@gmail.com>
2022-09-24 02:23:52 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const fixedTime = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(fixedTime));
+
+function currentTime(data) {
+  localStorage.setItem(fixedTime, data.seconds);
+  //   console.log('fixedTime:', localStorage.getItem(fixedTime));
+}
