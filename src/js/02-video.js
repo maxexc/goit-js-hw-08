49cc668381456c9d55c0bf55ea1e11a678564504
Maksym Solomko <max.exc7@gmail.com>
2022-09-26 01:11:38 +0300

    update & fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,8 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const fixedTime = 'videoplayer-current-time';
+const fixedTime = localStorage.getItem('videoplayerCurrentTime');
+// localStorage.fixedTime = 1;
 
 player.on('play', function () {
   console.log('played the video!');
@@ -13,11 +14,13 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
+if (fixedTime) {
+  player.setCurrentTime(fixedTime);
+}
+
 player.on('timeupdate', throttle(currentTime, 1000));
 
-player.setCurrentTime(localStorage.getItem(fixedTime));
-
 function currentTime(data) {
-  localStorage.setItem(fixedTime, data.seconds);
-  //   console.log('fixedTime:', localStorage.getItem(fixedTime));
+  localStorage.setItem('videoplayerCurrentTime', data.seconds);
+  // console.log('fixedTime:', localStorage.getItem(fixedTime));
 }
